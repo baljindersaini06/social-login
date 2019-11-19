6fea6492a9dbdcc1d9084a572ecc9d288c598c13
baljindersaini06 <saini4.baljinder@gmail.com>
2019-11-19 19:26:07 +0530

    second commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -93,8 +93,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "social-django",
+        'HOST': "localhost",
+        'USERNAME': 'root',
+        'PASSWORD': 'toor',
     }
 }
 
